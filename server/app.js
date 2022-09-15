ea93dda406c9b4d3d133494252f151f470f2002d
Godwin Opara <godwinopara62@yahoo.com>
2022-09-15 12:40:39 +0100

    set up enviroment variable using dotenv

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 // Your code here
+// require("dotenv").config()
 
 app.get('/', (req, res) => {
     res.send(process.env.SECRET_MESSAGE);
 });
+console.log(process.env.SECRET_MESSAGE)
 
 const port = process.env.PORT;
 app.listen(port, () => console.log('Server is listening on port', port));
